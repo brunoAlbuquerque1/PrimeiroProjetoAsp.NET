bac669f8d701e3d30c08744e5b9d0d82be296dfd
Bruno <bruno.albuquerque@mag-it.com.br>
2020-05-31 03:15:56 -0300

    Primeiro Projeto em Aps.NET

---
+++ PrimeiroProjetoTeste/CategoriaControllerTeste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiroProjetoTeste
+{
+  public  class CategoriaControllerTeste
+    {
+        public CategoriaControllerTeste()
+        {
+
+        }
+
+
+
+
+
+    }
+}
